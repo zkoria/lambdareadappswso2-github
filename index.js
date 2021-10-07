4f7c80daaa38ce27991efeef74444c9fdcb0f9fc
jonyx <jponcianovera@hotmailcom>
2021-10-07 14:40:04 -0500

    01

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-
-var https = require('https');
-
-exports.handler = function(event, context) {
-  console.log('start request to ' + event.url)
-  https.get(event.url, function(res) {
-    console.log("1-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-    console.log("Got response: " + res.statusCode);
-    context.succeed();
-
-    console.log("2-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-  }).on('error', function(e) {
-
-    console.log("3-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-    console.log("Got error: " + e.message);
-    context.done(null, 'FAILURE');
-  });
-
-  console.log('end request to ' + event.url);
+const http = require('https')
+exports.handler = async (event) => {
+    console.log('event.url')
+    return httprequest().then((data) => {
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    return response;
+    });
+};
+function httprequest() {
+     return new Promise((resolve, reject) => {
+        const options = {
+            host: 'https://sodev.anzen.com.mx',
+            path: '/getapps',
+            port: 8237,
+            method: 'GET'
+        };
+        const req = http.request(options, (res) => {
+          if (res.statusCode < 200 || res.statusCode >= 300) {
+                return reject(new Error('statusCode=' + res.statusCode));
+            }
+            var body = [];
+            res.on('data', function(chunk) {
+                body.push(chunk);
+            });
+            res.on('end', function() {
+                try {
+                    body = JSON.parse(Buffer.concat(body).toString());
+                } catch(e) {
+                    reject(e);
+                }
+                resolve(body);
+            });
+        });
+        req.on('error', (e) => {
+          reject(e.message);
+        });
+        // send the request
+       req.end();
+    });
 }
