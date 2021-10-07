fb94f492f1c8779631236fd3d25a747cb0ae1d21
jonyx <jponcianovera@hotmailcom>
2021-10-07 15:29:28 -0500

    01

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-const http = require('https')
-exports.handler = async (event) => {
-    return httprequest().then((data) => {
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(data),
-        };
-    return response;
+'use strict';
+
+const async = require('async');
+const https = require('https');
+
+module.exports.handler = function (event, context, callback) {
+
+    let body = "";
+    let countChunks = 0;
+
+    async.waterfall([
+        requestDataFromFeed,
+        // processBody,
+    ], (err, result) => {
+        if (err) {
+            console.log(err);
+            callback(err);
+        }
+        else {
+            const message = "Success";
+            console.log(result.body);
+            callback(null, message);
+        }
     });
+
+    function requestDataFromFeed(callback) {
+        const url = 'https://jsonplaceholder.typicode.com/todos';
+        console.log('Sending GET request to ${url}');
+        https.get(url, (response) => {
+            console.log('statusCode:', response.statusCode);
+            response.on('data', (chunk) => {
+                countChunks++;
+                body += chunk;
+            });
+            response.on('end', () => {
+                const result = {
+                    countChunks: countChunks,
+                    body: body
+                };
+                callback(null, result);
+            });
+        }).on('error', (err) => {
+            console.log(err);
+            callback(err);
+        });
+    }
 };
-function httprequest() {
-     return new Promise((resolve, reject) => {
-        const options = {
-            host: 'jsonplaceholder.typicode.com',
-            path: '/todos',
-            port: 443,
-            method: 'GET'
-        };
-        const req = http.request(options, (res) => {
-          if (res.statusCode < 200 || res.statusCode >= 300) {
-                return reject(new Error('statusCode=' + res.statusCode));
-            }
-            var body = [];
-            res.on('data', function(chunk) {
-                body.push(chunk);
-            });
-            res.on('end', function() {
-                try {
-                    body = JSON.parse(Buffer.concat(body).toString());
-                } catch(e) {
-                    reject(e);
-                }
-                resolve(body);
-            });
-        });
-        req.on('error', (e) => {
-          reject(e.message);
-        });
-        // send the request
-       req.end();
-    });
-}
