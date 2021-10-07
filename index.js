72a6e3f865e41b08765a8d5c610763127f9f525d
jonyx <jponcianovera@hotmailcom>
2021-10-07 14:30:14 -0500

    01

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var https = require('https');
 exports.handler = function(event, context) {
   console.log('start request to ' + event.url)
   https.get(event.url, function(res) {
+    console.log("1-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     console.log("Got response: " + res.statusCode);
     context.succeed();
+
+    console.log("2-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
   }).on('error', function(e) {
+
+    console.log("3-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     console.log("Got error: " + e.message);
     context.done(null, 'FAILURE');
   });
