2a6d97f6a6081b3f8019cb9d7590c3bb8088fc99
jonyx <jponcianovera@hotmailcom>
2021-10-07 00:55:26 -0500

    01

--- index.js
+++ index.js
@@ -18,8 +18,35 @@ const headers = {
 //exports.handler = async (event, context, callback) => {
 exports.handler = async (event) => {
 
-console.log("-----------------------------------------> A" )
+console.log("-----------------------------------------> A1" )
 
+var https = require('https');
+
+var options = {
+  host: 'sodev.anzen.com.mx',
+  //port: 8237,
+  port: 7777,
+  path: '/carbon',
+  method: 'GET'
+};
+
+console.log("-----------------------------------------> A2" )
+
+var req = https.request(options, function(res) {
+  console.log("statusCode: ", res.statusCode);
+  console.log("headers: ", res.headers);
+
+  res.on('data', function(d) {
+    process.stdout.write(d);
+  });
+});
+req.end();
+
+req.on('error', function(e) {
+  console.error(e);
+});
+
+console.log("-----------------------------------------> A3" )
 
 }
 
