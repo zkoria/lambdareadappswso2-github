b89188b89bd8797cee439d29e28ce96d4ce38e43
jonyx <jponcianovera@hotmailcom>
2021-10-07 01:13:48 -0500

    01

--- index.js
+++ index.js
@@ -14,39 +14,17 @@ const headers = {
     'Access-Control-Allow-Origin': '*',
 };
 
-// var contador=1;
-//exports.handler = async (event, context, callback) => {
-exports.handler = async (event) => {
 
-console.log("-----------------------------------------> A1" )
 
-var https = require('https');
+const https = require('https')
+let url = "https://sodev.anzen.com.mx:8237"
 
-var options = {
-  host: 'sodev.anzen.com.mx',
-  //port: 8237,
-  port: 7777,
-  path: '/carbon',
-  method: 'GET'
-};
-
-console.log("-----------------------------------------> A2" )
-
-var req = https.request(options, function(res) {
-  console.log("statusCode: ", res.statusCode);
-  console.log("headers: ", res.headers);
-
-  res.on('data', function(d) {
-    process.stdout.write(d);
-  });
-});
-req.end();
-
-req.on('error', function(e) {
-  console.error(e);
-});
-
-console.log("-----------------------------------------> A3" )
-
+exports.handler =  function(event, context, callback) {
+  https.get(url, (res) => {
+    callback(null, res.statusCode)
+  }).on('error', (e) => {
+    callback(Error(e))
+  })
 }
+
 
