3e850aa5475a302fc04b3f1109fe4ee464286643
jonyx <jponcianovera@hotmailcom>
2021-10-07 19:42:38 -0500

    01

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-
-
-
-
+const doRequest = () => new Promise((resolve, reject) => {
     const https = require('https');
     const options = {
         hostname: 'sodev.anzen.com.mx',
@@ -9,48 +6,31 @@
         path: '/getapps',
         method: 'GET'
     }
-
-
-const doRequest = () => new Promise((resolve, reject) => {
-console.log('A1')
     const req = https.request(options, res => {
-console.log('A2')
         var r = '';
         res.on('data', (d) => {
             r += d;
         });
         res.on('end', function () {
-console.log('A3')
             resolve(JSON.parse(r))
         });
     })
     req.on('error', (e) => {
         reject(e.message)
     })
-
-console.log('A4')
     req.end()
 })
 
-
 exports.handler = async (event) => {
-
-
-var m_response;
-await doRequest()
-.then((result) => {
-console.log('A0')
-	m_response = result;
-})
-.catch((err) => {
-	m_response = err;
-})
-
-return m_response;
-}
-
-
-
-
+    var response;
+    await doRequest()
+    .then((result) => {
+        response = result;
+    })
+    .catch((err) => {
+        response = err;
+    })
+    return response;
+};
 
 
